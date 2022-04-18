dd57def4984884fe9d42310e260b8d4aa5aa5487
Ömer Faruk Tomurcuk <63103371+Mrfrktmrck19@users.noreply.github.com>
2022-04-18 09:18:31 +0300

    Update BubbleSort.py

--- Brute Force Approach/Bubble Sort/BubbleSort.py
+++ Brute Force Approach/Bubble Sort/BubbleSort.py
@@ -1,1 +1,25 @@
 
+def bubbleSort(arr):
+    n = len(arr)
+ 
+    # Traverse through all array elements
+    for i in range(n):
+ 
+        # Last i elements are already in place
+        for j in range(0, n-i-1):
+ 
+            # traverse the array from 0 to n-i-1
+            # Swap if the element found is greater
+            # than the next element
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+ 
+ 
+# Driver code to test above
+arr = [7,2,6,8,10,1,3]
+ 
+bubbleSort(arr)
+ 
+print("Sorted array is:")
+for i in range(len(arr)):
+    print(arr[i], end=" ")
